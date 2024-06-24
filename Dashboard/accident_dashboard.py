55fa322b47b225cfa7a006df54f784e7d85bf37b
carizadias <95142358+carizadias@users.noreply.github.com>
2024-06-24 18:55:30 -0100

    second commit
    Addition of colab

--- Dashboard/accident_dashboard.py
+++ Dashboard/accident_dashboard.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:a31176890a87c00df99fd0e490691b7de6c6f4486fcd2ad96160c081af148cae
-size 12667
+oid sha256:339982c8d92ab9afad89d7dde87e3d9f36f04de3e01fe3dad469441ffffda3a2
+size 12710
