88937cce6b705af65b5528f8ef82f77cdbeb98f1
carizadias <95142358+carizadias@users.noreply.github.com>
2024-06-24 21:18:40 -0100

    fourth commit

--- Dashboard/accident_dashboard.py
+++ Dashboard/accident_dashboard.py
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:f3e6b45e070ddf233f7cea21934c1d08d3510f6d1d79f2b7cac37bc711488681
-size 12738
+oid sha256:339982c8d92ab9afad89d7dde87e3d9f36f04de3e01fe3dad469441ffffda3a2
+size 12710
